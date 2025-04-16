ce0e47a1404f698ffb9f93fede56ebb666412c26
layon <locosilia5@gmail.com>
2025-04-15 21:37:50 -0300

    criado a função excluir o nome da lista utilizando o codigo splice

--- main.js
+++ main.js
@@ -32,9 +32,10 @@ function salvarUser(){
 // função para preencher a lista de cadatro
 
 function criaLista(){
+
     let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
     for(let i = 0; i <= (dadoslista.length - 1); i++){
-        tabela += "<tr><td>" + dadoslista[i] + "</td><td><button class='btn btn-warning'>Editar</button><button class='btn btn-danger'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadoslista[i] + "</td><td><button class='btn btn-warning' onclick=''>Editar</button><button class='btn btn-danger' onclick = 'excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
 }
@@ -42,4 +43,6 @@ function criaLista(){
 // função ára excluir nome da lista
 function excluir(i){
     dadoslista.splice((i-1), 1);
+    document.getElementById('tabela').deleteRow(i);
+    console.log(dadoslista);
 }
